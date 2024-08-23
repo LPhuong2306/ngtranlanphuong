51ea204ac4ff899e3d04954e185a47e8cc8b7ec6
NguyenTranLanPhuong23666431 <nguyennguyenlanphuong@gmail.com>
2024-08-23 16:03:33 +0700

    Add files via upload

--- buoi6/2.py
+++ buoi6/2.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Aug 22 18:27:57 2024
 
-@author: My pc
+@author: lanphuong
 """
 str1 = ("Đại học Quốc gia,\nKhu phố 6,\nP. Linh Trung,\nQ. Thủ Đức,\nTp. HCM")
 str1 = str1.replace(",","")
