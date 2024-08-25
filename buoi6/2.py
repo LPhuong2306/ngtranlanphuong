a4853f985b47a230505343edc13ef0cbb252aa6a
NguyenTranLanPhuong23666431 <nguyennguyenlanphuong@gmail.com>
2024-08-25 13:42:23 +0700

    Add files via upload

--- buoi6/2.py
+++ buoi6/2.py
@@ -4,10 +4,9 @@ Created on Thu Aug 22 18:27:57 2024
 
 @author: lanphuong
 """
-str1 = ("Đại học Quốc gia,\nKhu phố 6,\nP. Linh Trung,\nQ. Thủ Đức,\nTp. HCM")
-str1 = str1.replace(",","")
-str1 = str1.replace("P. ","")
-str1 = str1.replace("Tp. ","")
-str1 = str1.replace("Q. ","")
-print(str1)
+input_string = "Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM"
+
+substrings = input_string.split(",")
+for s in substrings:
+    print(s.strip())
 
